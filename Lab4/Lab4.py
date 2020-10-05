f2133fc704e40cb67d9fc8f7341b71dad7a83a0d
juanesleal <juanleal@cmail.carleton.ca>
2020-10-05 16:29:12 -0400

    Lab4 programming task

---
+++ Lab4/Lab4.py
@@ -0,0 +1,26 @@
+import http.client
+import urllib.parse
+import time
+key = '54CDA6FJF4RCJ92C'
+# Put your API Key here IIUQ8XBFN7K9WG2A
+#54CDA6FJF4RCJ92C
+def lab4():
+        field1 = 'juanleal@cmail.carleton.ca'
+        field2 = 'L2-M-7'
+        field3 = 'c'
+        param = urllib.parse.urlencode({'field1': field1, 'field2': field2,'field3': field3,'key':key }) 
+        header= {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
+        conn = http.client.HTTPConnection("api.thingspeak.com:80")
+        
+        try:
+            conn.request("POST", "/update", param, header)
+            response = conn.getresponse()
+            print(field1)
+            print(field2)
+            print(field3)
+            data = response.read()
+            conn.close()
+        except:
+            print("connection failed")
+if __name__ == "__main__":
+                lab4()
